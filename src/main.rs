76b362cefe5e17708bc79213c422e11ae7be853b
elkhoudiry <ahmedelkhodery2@Gmail.com>
2023-12-05 22:31:28 +0200

    solve day 2 part 2

--- src/main.rs
+++ src/main.rs
@@ -26,4 +26,12 @@ fn main() {
         2176,
         year_2023::day_2::first::run("/src/year_2023/day_2/first_input")
     );
+    assert_eq!(
+        2286,
+        year_2023::day_2::second::run("/src/year_2023/day_2/first_sample")
+    );
+    assert_eq!(
+        63700,
+        year_2023::day_2::second::run("/src/year_2023/day_2/first_input")
+    );
 }
